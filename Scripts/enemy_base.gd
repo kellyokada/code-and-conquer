2fb91cc21a3d69beab96fe86bd14ebb238240516
clowesait <cody.lowe@edu.sait.ca>
2025-10-29 22:54:16 -0600

    Currency is displayed properly and should increment
    Can't test the incrementing until the towers are fixed but it all should be good

--- Scripts/enemy_base.gd
+++ Scripts/enemy_base.gd
@@ -19,9 +19,10 @@ func _ready():
 func _process(_delta: float) -> void:
 	progress_ratio += _delta * speed
 	var place = get_progress_ratio()
-	if place >= 1:
+	if place >= 0.999: #Temp fix for the looping issue
 		GameState.damage(player_damage) #Damages player when it reaches end
 		queue_free()
 		
 	if health <= 0:
-		get_parent().get_parent().queue_free()
+		GameState.add_currency(killReward)
+		queue_free()
