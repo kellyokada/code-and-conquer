7a8207b761aa2a0151bcd816e10cec7d9d758b19
J-Wales <151964658+J-Wales@users.noreply.github.com>
2025-10-30 10:17:33 -0600

    Level select, again?

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -8,11 +8,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
+	$VBoxContainer/StartButton.set_process(false)
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_select.tscn")
 
 
 func _on_quit_button_pressed() -> void:
