fe95139f229ace774fc9e90bea1badff72a1dbfd
foxdog2104 <nyxreidy@gmail.com>
2025-11-06 10:45:44 -0700

    Revert "Tower placement"

--- Scripts/wizard_tower_panel.gd
+++ Scripts/wizard_tower_panel.gd
@@ -2,38 +2,35 @@ extends Panel
 
 @onready var tower = preload("res://Scenes/wizard.tscn")
 var currTile
-var actual_tower
-var can_place
-var can_add_tower
 
 func _on_gui_input(event: InputEvent) -> void:
+	var tempTower = tower.instantiate()
 	
 	if event is InputEventMouseButton and event.button_mask == 1:
-		var temp_tower = tower.instantiate()
-		actual_tower = temp_tower
-		var path = get_tree().get_root().get_node("Main/Towers")
-		path.add_child(actual_tower)
-		can_place = true 
-		actual_tower.global_position = event.global_position
+		add_child(tempTower)
+		tempTower.global_position = event.global_position
+		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
 		
-		actual_tower.scale = Vector2(1.3,1.3)
-
-	elif event is InputEventMouseButton and event.button_mask == 0 and actual_tower:
-		if can_add_tower :
-			actual_tower.activate_tower()
-			actual_tower.global_position = event.global_position
-			can_place = false
-		else: 
-			actual_tower.queue_free()
-		actual_tower = null 
-func _process(delta: float) -> void:
-	can_add_tower = can_place and actual_tower and can_add_tower
-	if can_place and actual_tower :
-		actual_tower.global_position = get_global_mouse_position().snapped(Vector2(32,32))
-		var areas= actual_tower.get_available_areas()
-		if areas.size() > 0 :
-			can_add_tower = false
-			actual_tower.modulate = Color.RED
-		else:
-			can_add_tower = true
-			actual_tower.modulate = Color.GREEN
+		tempTower.scale = Vector2(1.3,1.3)
+		
+	elif event is InputEventMouseMotion and event.button_mask == 1:
+		if get_child_count() > 1:
+			get_child(1).global_position = event.global_position
+		
+			var mapPath = get_tree().get_root().get_node("Main/TileMap")
+			var tile = mapPath.local_to_map(get_global_mouse_position())
+			currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+			
+			if (currTile == Vector2i(0,0)):
+				get_child(1).get_node("Area").modulate = Color(0,255,0)
+			#else:
+				#get_child(1).get_node("Area").modulate = Color(255,0,255)
+			
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
+		var path = get_tree().get_root().get_node("Main/Towers")
+		
+		path.add_child(tempTower)
+		tempTower.global_position = event.global_position
+		tempTower.get_node("Area").hide()
